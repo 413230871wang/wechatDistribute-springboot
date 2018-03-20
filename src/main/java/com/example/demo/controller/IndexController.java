192b04d7575016d90f84c4c7aa1dba51dab58765
coder_wanglei <wanglei19930915@126.com>
2018-03-20 18:53:01 +0800

    20180320 by lay

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.BookBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+    @Value(value = "${books.author}")
+    private String bookAuthor;
+    @Value("${books.name}")
+    private String bookName;
+    @Value("${books.pinyin}")
+    private String bookPinYin;
+
+    @Autowired
+    private BookBean bookBean;
+
     @RequestMapping("/index")
     public String index(){
         System.out.println("这是一个测试类");
+        System.out.println("Hello Spring Boot! The BookName is "+bookName+";and Book Author is "+bookAuthor+";and Book PinYin is "+bookPinYin);
+        System.out.println("Hello Spring Boot! The BookName is "+bookBean.getName()+";and Book Author is "+bookBean.getAuthor()+";and Book price is "+bookBean.getPrice());
         return "index";
     }
 }
