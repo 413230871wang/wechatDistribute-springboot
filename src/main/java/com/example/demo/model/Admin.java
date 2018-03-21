24f11c74a97d95fc3bfb05b86829e22449a9f0a6
coder_wanglei <wanglei19930915@126.com>
2018-03-21 14:05:11 +0800

    20180321 by lay

---
+++ src/main/java/com/example/demo/model/Admin.java
@@ -0,0 +1,115 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class Admin {
+    private Integer id;
+
+    private Date createdate;
+
+    private Boolean deleted;
+
+    private Integer version;
+
+    private Integer juri;
+
+    private String lastloginip;
+
+    private Date lastlogintime;
+
+    private Integer logincount;
+
+    private String name;
+
+    private String password;
+
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Integer getJuri() {
+        return juri;
+    }
+
+    public void setJuri(Integer juri) {
+        this.juri = juri;
+    }
+
+    public String getLastloginip() {
+        return lastloginip;
+    }
+
+    public void setLastloginip(String lastloginip) {
+        this.lastloginip = lastloginip == null ? null : lastloginip.trim();
+    }
+
+    public Date getLastlogintime() {
+        return lastlogintime;
+    }
+
+    public void setLastlogintime(Date lastlogintime) {
+        this.lastlogintime = lastlogintime;
+    }
+
+    public Integer getLogincount() {
+        return logincount;
+    }
+
+    public void setLogincount(Integer logincount) {
+        this.logincount = logincount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
